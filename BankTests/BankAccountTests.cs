cfa531d74ffc4abe64cd1e251438187028f45a6d
Makayla Konefke <konefkm@mail.nmc.edu>
2026-08-31 02:20:55 -0400

    Add project files.

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,32 @@
+﻿// The 'using' statement for Test Tools is in GlobalUsings.cs
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
+        }
+    }
+}
